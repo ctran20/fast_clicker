28c856e79bae949c1d4226d11c898b22ad323947
Cat Tran <54409102+ctran20@users.noreply.github.com>
2022-07-02 12:32:59 -0400

    Extremely fast clicks

--- fast_clicker.py
+++ fast_clicker.py
@@ -1,6 +1,16 @@
-import pyautogui
+from pynput import mouse
+from pynput.mouse import Controller, Button
 
-for i in range(800):
-  if i % 50 == 0:
-    print(i)
-  pyautogui.click(400, 600)
+mouse = Controller()
+length = 21
+start = int(1/length)
+
+for cycle in range(length):
+  # Click 5k times
+  for _ in range(5000):
+    mouse.click(Button.left)
+
+  # Print progress   
+  print(f'{start + int(cycle/length * 100)}%')
+  
+print('Done!')
