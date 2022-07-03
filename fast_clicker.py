4a23b88c39c92c520c6bf11b284fbbb8da5b36ea
Cat Tran <54409102+ctran20@users.noreply.github.com>
2022-07-03 12:12:35 -0400

    Adjust to proper click frequency

--- fast_clicker.py
+++ fast_clicker.py
@@ -1,16 +1,23 @@
 from pynput import mouse
 from pynput.mouse import Controller, Button
+import time
 
 mouse = Controller()
-length = 21
+length = 120
 start = int(1/length)
 
 for cycle in range(length):
   # Click 5k times
-  for _ in range(5000):
+  for _ in range(60):
     mouse.click(Button.left)
+    mouse.click(Button.left)
+    mouse.click(Button.left)
+    mouse.click(Button.left)
+    mouse.click(Button.left)
+    time.sleep(0.001)
 
-  # Print progress   
-  print(f'{start + int(cycle/length * 100)}%')
+  # Print progress
+  if(cycle % 5 == 0):   
+    print(f'{length - cycle}s')
   
 print('Done!')
